0851fc1f8edcfaf66cc8448f11663b7587e42942
stbanas <pio.banasik@gmail.com>
2020-03-21 11:48:07 +0100

    Reconstruction of the "wlej" method, inserting new variable "ileLitrow ", chenge of the veriable "pojemnoscMax";

--- src/com/company/Butelka.java
+++ src/com/company/Butelka.java
@@ -3,17 +3,23 @@ package com.company;
     public class Butelka {
 
         private double pojemnoscMax;
+        private double ileLitrow;
         String nazwa;
 
-        Butelka(double pojemnoscMax, String nazwa)
+        Butelka(double pojemnoscMax, double ileLitrow, String nazwa)
         {
             this.pojemnoscMax = pojemnoscMax;
+            this.ileLitrow = ileLitrow;
             this.nazwa = nazwa;
         }
 
         double getIleZostalo()
             {
              return pojemnoscMax;
+            }
+         double getIleZostaloWewnButelki()
+            {
+             return getIleZostalo() - pojemnoscMax;
             }
 
 //         void sytuacjaBiezaca()
@@ -24,15 +30,15 @@ package com.company;
 
         void wlej(double ilosc)
             {
-            if (ilosc < this.pojemnoscMax)
-                this.pojemnoscMax -= ilosc;
+            if (ilosc <= this.pojemnoscMax - ileLitrow)
+                this.ileLitrow += ilosc;
             else
                  System.out.println("Za mało miejsca w butelce. Próbę wlania anulowano");
             }
 
         void wylej(double ilosc)
         {
-            if (ilosc < pojemnoscMax )
+            if (ilosc > getIleZostaloWewnButelki())
                 this.pojemnoscMax += ilosc;
             else
                 System.out.println("Nie ma tyle płynu w butelce.");
@@ -45,46 +51,45 @@ package com.company;
         int i = 1;
         double[] ltr = {1, 2, 3, 4, 5, 12, 15}; /* tu mam 7 wartości */
         String[] nazwa = {"One", "Two", "Three", "Four", "Five", "Twelve", "Fifteen"};/* tu też mam 7 wartości */
-//        for (i = 1; i < 8; i++) {
-//            butelka[i] = new Butelka(ltr[i - 1], nazwa[i - 1]);
+        double[] ltrPoczatkowe = {0, 0, 0, 0, 0, 0, 0};
 
             for (i = 1; i < 8; i++) {
-                butelka[i] = new Butelka(ltr[i - 1], nazwa[i - 1]);/* a tu mam 7 wartości, ale  przy przesuniętym w lewo indeksie o 1 */
+                butelka[i] = new Butelka(ltr[i - 1], ltrPoczatkowe[i-1], nazwa[i - 1]);/* a tu mam 7 wartości, ale  przy przesuniętym w lewo indeksie o 1 */
 
                 System.out.println( "Butelka: " + butelka[i].nazwa +  " \t \t : " + butelka[i].pojemnoscMax + "  =  pojemność całkowita.     "
-                                  +  (butelka[i].pojemnoscMax - butelka[i].getIleZostalo()) + "  = początkowa zawartość płynu");
+                                  +  (butelka[i].ileLitrow) + "  = początkowa zawartość płynu");
 
-                System.out.println("-----------------------------------------------------------------------------------------------------");
+                System.out.println("***************************************************************************************************");
 
 
             }
             butelka[1].wlej(0.4);
-            System.out.println(" W butelce  " + butelka[1].nazwa + " jest płyn w ilości " + (ltr[0] - butelka[1].getIleZostalo())
-                               + " litrów. Zostało " + butelka[1].getIleZostalo() + " litrów miejsca.");
+            System.out.println(" W butelce '" + butelka[1].nazwa + "' jest płyn w ilości " + (butelka[1].ileLitrow)
+                               + " litrów. Zostało " + (butelka[1].pojemnoscMax - butelka[1].ileLitrow) + " litrów miejsca.");
             System.out.println("-------------------------------------------------------------------------------------------------");
 
             butelka[4].wlej(3);
-            System.out.println(" W butelce " + butelka[4].nazwa + " jest płyn w ilości " + (ltr[3] - butelka[4].getIleZostalo())
-                    + " litrów. Zostało " + butelka[4].getIleZostalo() + " litrów miejsca.");
+            System.out.println(" W butelce '" + butelka[4].nazwa + "' jest płyn w ilości " + (butelka[4].ileLitrow)
+                    + " litrów. Zostało " + (butelka[4].pojemnoscMax - butelka[4].ileLitrow) + " litrów miejsca.");
             System.out.println("-------------------------------------------------------------------------------------------------");
 
             butelka[7].wlej(13.0);
-            System.out.println(" W butekce  " + butelka[7].nazwa + " jest płyn w ilości " + (ltr[6] - butelka[7].getIleZostalo())
-                    + " litrów. Zostało " + butelka[7].getIleZostalo() + " litrów miejsca.");
+            System.out.println(" W butekce '" + butelka[7].nazwa + "' jest płyn w ilości " + (butelka[7].ileLitrow)
+                    + " litrów. Zostało " + (butelka[7].pojemnoscMax - butelka[7].ileLitrow) + " litrów miejsca.");
             System.out.println("-------------------------------------------------------------------------------------------------");
 
-            butelka[4].wylej(3); /* można wylać za duzo - trzeba ograniczyć*/
-
-            System.out.println(" W butelce " + butelka[4].nazwa + " jest płyn w ilości " + (ltr[3]  - butelka[4].getIleZostalo())
-                    + " litrów. Zostało " + butelka[4].getIleZostalo() + " litrów miejsca.");
-            System.out.println("-------------------------------------------------------------------------------------------------");
-
-            butelka[7].wylej(3.0);
-            System.out.println("  W butelce " + butelka[7].nazwa + " jest płyn w ilości " + (ltr[6] - butelka[7].getIleZostalo())
-                    +" litrów. Zostało " + butelka[7].getIleZostalo() + " litrów miejsca.");
-            System.out.println("-------------------------------------------------------------------------------------------------");
-
-
+//            butelka[4].wylej(5); /* można wylać za duzo - trzeba ograniczyć*/
+//
+//            System.out.println(" W butelce " + butelka[4].nazwa + " jest płyn w ilości " + (ltr[3]  - butelka[4].getIleZostalo())
+//                    + " litrów. Zostało " + butelka[4].getIleZostalo() + " litrów miejsca.");
+//            System.out.println("-------------------------------------------------------------------------------------------------");
+//
+//            butelka[7].wylej(7.0);
+//            System.out.println("  W butelce " + butelka[7].nazwa + " jest płyn w ilości " + (ltr[6] - butelka[7].getIleZostalo())
+//                    +" litrów. Zostało " + butelka[7].getIleZostalo() + " litrów miejsca.");
+//            System.out.println("-------------------------------------------------------------------------------------------------");
+//
+//        System.out.println(butelka[4].getIleZostaloWewnButelki());
         }
     }
 
