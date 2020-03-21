48e9c9c47361c66e22ebbf51d5a06446cbcdc676
stbanas <pio.banasik@gmail.com>
2020-03-21 18:07:13 +0100

    To the Butelka cod the new method "przelej" is now implemented; the logic "if" in the body block is not completed yet;

--- src/com/company/Butelka.java
+++ src/com/company/Butelka.java
@@ -29,7 +29,7 @@ package com.company;
             if (ilosc <= this.pojemnoscMax - this.ileLitrow)
                 this.ileLitrow += ilosc;
             else
-                 System.out.println("Za mało miejsca w butelce. Próbę wlania anulowano");
+                 System.out.println(" Za mało miejsca w butelce." + ilosc + " litrow to zbyt dużo. Próbę wlania anulowano");
             }
 
         void wylej(double ilosc)
@@ -37,7 +37,17 @@ package com.company;
             if (ilosc < (this.ileLitrow))
                 this.ileLitrow -= ilosc;
             else
-                System.out.println("Nie ma tyle płynu w butelce.");
+                System.out.println(" Próba wylania "+ ilosc + " litrów nie powiodła się. Nie ma tyle płynu w butelce. Operacja została anulowana.");
+        }
+        void przelej(double ilosc, Butelka doKtorajButelki)
+        {
+            if (ilosc < (this.ileLitrow))
+                {
+                this.wylej(ilosc);
+                doKtorajButelki.wlej(ilosc);
+                }
+            else
+                System.out.println(" Nie ma miejsca w butelce ."+ doKtorajButelki.nazwa);
         }
 
     public static void main(String[] args) {
@@ -52,17 +62,17 @@ package com.company;
             for (i = 1; i < 8; i++) {
                 butelka[i] = new Butelka(ltr[i - 1], ltrPoczatkowe[i-1], nazwa[i - 1]);/* a tu mam 7 wartości, ale  przy przesuniętym w lewo indeksie o 1 */
 
-                System.out.println( "Butelka: " + butelka[i].nazwa +  " \t \t : " + butelka[i].pojemnoscMax + "  =  pojemność całkowita.     "
+                System.out.println( "Butelka: '" + butelka[i].nazwa +  "' \t \t : " + butelka[i].pojemnoscMax + "  =  pojemność całkowita.     "
                                   +  (butelka[i].ileLitrow) + "  = początkowa zawartość płynu");
 
                 System.out.println("***************************************************************************************************");
 
 
             }
-            butelka[1].wlej(0.4);
-            System.out.println(" W butelce '" + butelka[1].nazwa + "' jest płyn w ilości " + (butelka[1].ileLitrow)
-                               + " litrów. Zostało " + (butelka[1].pojemnoscMax - butelka[1].ileLitrow) + " litrów miejsca.");
-            System.out.println("-------------------------------------------------------------------------------------------------");
+//            butelka[1].wlej(0.4);
+//            System.out.println(" W butelce '" + butelka[1].nazwa + "' jest płyn w ilości " + (butelka[1].ileLitrow)
+//                               + " litrów. Zostało " + (butelka[1].pojemnoscMax - butelka[1].ileLitrow) + " litrów miejsca.");
+//            System.out.println("-------------------------------------------------------------------------------------------------");
 
             butelka[4].wlej(3);
             System.out.println(" W butelce '" + butelka[4].nazwa + "' jest płyn w ilości " + (butelka[4].ileLitrow)
@@ -75,14 +85,22 @@ package com.company;
             System.out.println("-------------------------------------------------------------------------------------------------");
 
             butelka[4].wylej(1); /* można wylać za duzo - trzeba ograniczyć*/
-            System.out.println(" W butelce " + butelka[4].nazwa + " jest płyn w ilości " + (butelka[4].ileLitrow)
+            System.out.println(" W butelce '" + butelka[4].nazwa + "' jest płyn w ilości " + (butelka[4].ileLitrow)
                     + " litrów. Zostało " + (butelka[4].pojemnoscMax - butelka[4].ileLitrow) + " litrów miejsca.");
             System.out.println("-------------------------------------------------------------------------------------------------");
 
             butelka[7].wylej(17.0);
-            System.out.println("  W butelce " + butelka[7].nazwa + " jest płyn w ilości " + (butelka[7].ileLitrow)
+            System.out.println("  W butelce '" + butelka[7].nazwa + "' jest płyn w ilości " + (butelka[7].ileLitrow)
                     +" litrów. Zostało " + (butelka[7].pojemnoscMax - butelka[7].ileLitrow) + " litrów miejsca.");
             System.out.println("-------------------------------------------------------------------------------------------------");
+
+            butelka[7].przelej( 8, butelka[4]);
+            System.out.println(" W butelce '" + butelka[4].nazwa + "' jest płyn w ilości " + (butelka[4].ileLitrow)
+                + " litrów. Zostało " + (butelka[4].pojemnoscMax - butelka[4].ileLitrow) + " litrów miejsca.");
+            System.out.println(" W butelce '" + butelka[7].nazwa + "' jest płyn w ilości " + (butelka[7].ileLitrow)
+                +" litrów. Zostało " + (butelka[7].pojemnoscMax - butelka[7].ileLitrow) + " litrów miejsca.");
+// Tu jeszcze nie działa do końca - co prawda nie da się wlać do "four" do za mało miejsca, ale z "Fifeteen" dało się wylać całą, zadeklarowaną "ilosc"
+//  Trzeba dołożyć blokadę : zanim zacznę wylewać ze "źródła", to muszę sprawdzić miejsce w "doKtorejButelki.nazwa"
 
         }
     }
